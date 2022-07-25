0e6e8af3ee363d7f15f3cc4a7d5803a58a1f6537
sonalS2210 <sonalsingh2210@gmail.com>
2022-07-25 20:22:59 +0530

    Update selected columns for prediction

--- predictor.py
+++ predictor.py
@@ -7,7 +7,13 @@ def predict (data, model_path):
     model_path: path to the model (string) 
     ''' 
     model = pickle.load(open(model_path, 'rb')) 
-    data = pd.read_csv(data)
+    data = pd.read_json(data)
+    selected_cols = ['Lube Oil Tank Temperature', 'Pump Journal 2 Bearing Temperature',
+       'Pump Suction Strainer Differential Pressure',
+       'Motor Current Phase A', 'Pump Shaft Speed',
+       'Heat Recovery System Header Mass Flow', 'Motor Voltage',
+       'Pump Suction Temperature', 'Pump Journal 1 Bearing Temperature',
+       'Pump Thrust Bearing Temperature 1']
 
     data = data[selected_cols]
     prediction = model.predict(data)  
